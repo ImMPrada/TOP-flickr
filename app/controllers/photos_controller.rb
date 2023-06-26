b41b472fdf0eff8ad7c1fd3adc75a94d9a440512
immprada <im.mprada@gmail.com>
2023-06-26 11:12:36 -0500

    root to photos#index

---
+++ app/controllers/photos_controller.rb
@@ -0,0 +1,5 @@
+class PhotosController < ApplicationController
+  before_action :authenticate_user!
+
+  def index; end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "photos#index"
+
+  resources :photos, only: [:index, :show, :new, :create, :destroy]
 end
