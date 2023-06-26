8abf1c751cd5e763ed857b6d955df45aa45b316d
immprada <im.mprada@gmail.com>
2023-06-26 12:21:47 -0500

    setup system to allow flickr authentication

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -25,6 +25,7 @@ Devise.setup do |config|
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
   config.mailer_sender = 'please-change-me-at-config-initializers-devise@example.com'
+  config.omniauth :flickr, ENV['FLICKR_KEY'], ENV['FLICKR_SECRET'], { scopes: 'read,write,delete' }
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
