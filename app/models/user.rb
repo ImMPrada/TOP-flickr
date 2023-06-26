72855d6ab8be5cb6ebbb08606046d059e8eca9cd
immprada <im.mprada@gmail.com>
2023-06-26 14:00:42 -0500

    add flickr user info to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  before_validation :set_avatar_url, on: :create
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -7,9 +9,20 @@ class User < ApplicationRecord
 
   validates :username, presence: true, uniqueness: true
   validates :flickr_uid, uniqueness: true, allow_nil: true
-
+  validates :avatar_url, presence: true
 
   def self.find_from_flickr_oauth(auth)
     find_by(flickr_uid: auth.uid)
+  end
+
+  def add_flickr(auth)
+    self.flickr_uid = auth.uid
+    self.flickr_username = auth.info.nickname
+
+    save!
+  end
+
+  def set_avatar_url
+    self.avatar_url = "https://api.dicebear.com/6.x/bottts/svg?seed=#{username}"
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_26_173045) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_26_175058) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -25,6 +25,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_26_173045) do
     t.string "username", null: false
     t.string "flickr_username"
     t.string "flickr_uid"
+    t.string "avatar_url", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["flickr_uid"], name: "index_users_on_flickr_uid", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
